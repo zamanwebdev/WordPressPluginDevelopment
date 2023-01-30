708d3869b7b1fbd560897e5e93a69e7eda3c7d8a
Syed Zaman Mostafiz <zamanwebdev@gmail.com>
2023-01-31 00:29:27 +0600

    WordPress Plugin Development Learning Start

---
+++ simple-scroll-to-top-wp.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * Plugin Name:       Simple Scroll To Top WP
+	 * Plugin URI:        https://wordpress.org/plugins/simple-scroll-to-top-wp/
+	 * Description:       Simple Scroll to top plugin will help you to enable Back to Top button to your WordPress website.
+	 * Version:           1.0.0
+	 * Requires at least: 5.2
+	 * Requires PHP:      7.2
+	 * Author:            Syed Zaman
+	 * Author URI:        http://webdev.mourosizamanit.com/
+	 * License:           GPL v2 or later
+	 * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+	 * Update URI:        https://github.com/shovoalways
+	 * Text Domain:       sstt
+	*/
+?>
